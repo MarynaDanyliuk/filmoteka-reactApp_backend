5d8ef1cf4a2f93571c2c5f6a0229299146d2f495
Mary Danyliuk <marydanyliuk@gmail.com>
2024-02-21 10:50:43 +0200

    try vercel deploy

--- rotes/api/movies.js
+++ rotes/api/movies.js
@@ -6,8 +6,11 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   const result = await movies.listMovies();
-  res.json(result);
+  return res.status(200).json(result);
 });
+
+// title: "Express Testing",
+// message: "The app is working properly!",
 
 // router.get("/:movieId", async (req, res, next) => {
 //   res.json({ message: "template message" });
