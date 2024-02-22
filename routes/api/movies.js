2098c3c7231f628b6298aaa9e53c82b41fe22461
Mary Danyliuk <marydanyliuk@gmail.com>
2024-02-22 10:42:40 +0200

    upd

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,16 +1,16 @@
 const express = require("express");
 
-const movies = require("../models/movies");
+// const movies = require("../models/movies");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  // return res.status(200).json({
-  //   title: "Express Testing",
-  //   message: "The app is working properly!",
-  // });
-  const result = await movies.listMovies();
-  return res.status(200).json(result);
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+  // const result = await movies.listMovies();
+  // return res.status(200).json(result);
 });
 
 module.exports = router;
